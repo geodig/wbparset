03da99e9b15bd045f9d06c2212650eaaf4c73874
geodig <46333922+geodig@users.noreply.github.com>
2023-03-16 11:32:23 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -52,103 +52,107 @@ if submit:
     st.session_state["dfstrati"] = dfstrati
 
 with col2:
-    st.write("# Borehole/CPT Location")
-
-    # SESSION STATE ===============================================================
-    wb = st.session_state['wb']
-    utm_zone = st.session_state['utmz'] 
-    utm_hemi = st.session_state['utmh']
-
-    # READING INPUT SPREADSHEET ===================================================
-    sheet_bhmgr = wb['borehole_manager']
-    sheet_cptmgr = wb['cpt_manager']
-
-    BH_n = sheet_bhmgr.max_row - 1
-    BH_ID,BH_X,BH_Y,BH_Z,BH_GWL,BH_cat = [],[],[],[],[],[]
-    for i in range(BH_n):
-        BH_ID.append(sheet_bhmgr.cell(i+2,2).value)
-        BH_X.append(sheet_bhmgr.cell(i+2,3).value)
-        BH_Y.append(sheet_bhmgr.cell(i+2,4).value)
-        BH_Z.append(sheet_bhmgr.cell(i+2,5).value)
-        BH_GWL.append(sheet_bhmgr.cell(i+2,6).value)
-        BH_cat.append("BH")
-
-    BH_X = np.array(BH_X)
-    BH_Y = np.array(BH_Y)
-    BH_Z = np.array(BH_Z)
-    BH_GWL = np.array(BH_GWL)
-    BH_ID = np.array(BH_ID)
-    BH_cat = np.array(BH_cat)
-
-    CPT_n = sheet_cptmgr.max_row - 1
-    CPT_ID, CPT_X, CPT_Y, CPT_Z, CPT_GWL, CPT_cat = [],[],[],[],[],[]
-    for i in range(CPT_n):
-        CPT_ID.append(sheet_cptmgr.cell(i+2,2).value)
-        CPT_X.append(sheet_cptmgr.cell(i+2,3).value)
-        CPT_Y.append(sheet_cptmgr.cell(i+2,4).value)
-        CPT_Z.append(sheet_cptmgr.cell(i+2,5).value)
-        CPT_GWL.append(sheet_cptmgr.cell(i+2,6).value) 
-        CPT_cat.append("CPT")
-
-    CPT_X = np.array(CPT_X)
-    CPT_Y = np.array(CPT_Y)
-    CPT_Z = np.array(CPT_Z)
-    CPT_GWL = np.array(CPT_GWL)
-    CPT_ID = np.array(CPT_ID)
-    CPT_cat = np.array(CPT_cat)
-
-    ALL_X = np.concatenate((BH_X, CPT_X))
-    ALL_Y = np.concatenate((BH_Y, CPT_Y))
-    ALL_Z = np.concatenate((BH_Z, CPT_Z))
-    ALL_GWL = np.concatenate((BH_GWL, CPT_GWL))
-    ALL_ID = np.concatenate((BH_ID, CPT_ID))
-    ALL_cat = np.concatenate((BH_cat, CPT_cat))
-
-    ALL_X = [i for i in ALL_X if i != None]
-    ALL_Y = [i for i in ALL_Y if i != None]
-    ALL_Z = [i for i in ALL_Z if i != None]
-    ALL_GWL = [i for i in ALL_GWL if i != None]
-    ALL_ID = [i for i in ALL_ID if i != None]
-    ALL_cat = [i for i in ALL_cat if i != None]
-
-    data = {'ID' : ALL_ID,
-            'X_UTM' : ALL_X,
-            'Y_UTM' : ALL_Y,
-            'Z' : ALL_Z,
-            'GWL' : ALL_GWL,
-            'CLASS' : ALL_cat}
-
-    df = pd.DataFrame(data)
-
-    latlon, lat, lon = [],[],[]
-    for i in range(len(ALL_X)):
-        #print(df['ID'].iloc[i])
-        if utm_hemi == 'North':
-            hemis = True
-        elif utm_hemi == 'South':
-            hemis = False
-        latlon.append(utm.to_latlon(ALL_X[i], ALL_Y[i], utm_zone, northern=hemis))
-        lat.append(latlon[i][0])
-        lon.append(latlon[i][1])
-
-    df['lat']=lat
-    df['lon']=lon
-
-    latmean = df['lat'].mean()
-    lonmean = df['lon'].mean()
-    location = [latmean, lonmean]
-    m = folium.Map(location=location, zoom_start=15
-                    # , tiles="CartoDB positron"
-                   )
-
-    icons = []
-    for i in range(0,len(df)):
-        if df['CLASS'].iloc[i] == "BH":
-            icons.append(folium.Icon(color='black'))
-        elif df['CLASS'].iloc[i] == "CPT":
-            icons.append(folium.Icon(color='red'))
-
-    for i in range(0,len(df)):
-        folium.Marker([df['lat'].iloc[i], df['lon'].iloc[i]], popup=df['ID'].iloc[i], icon=icons[i]).add_to(m)
-
-    output = st_folium(m, width=1200, height=800, returned_objects=[])
+    if "wb" in st.session_state:
+        st.write("# Borehole/CPT Location")
+    
+        # SESSION STATE ===============================================================
+        wb = st.session_state['wb']
+        utm_zone = st.session_state['utmz'] 
+        utm_hemi = st.session_state['utmh']
+    
+        # READING INPUT SPREADSHEET ===================================================
+        sheet_bhmgr = wb['borehole_manager']
+        sheet_cptmgr = wb['cpt_manager']
+    
+        BH_n = sheet_bhmgr.max_row - 1
+        BH_ID,BH_X,BH_Y,BH_Z,BH_GWL,BH_cat = [],[],[],[],[],[]
+        for i in range(BH_n):
+            BH_ID.append(sheet_bhmgr.cell(i+2,2).value)
+            BH_X.append(sheet_bhmgr.cell(i+2,3).value)
+            BH_Y.append(sheet_bhmgr.cell(i+2,4).value)
+            BH_Z.append(sheet_bhmgr.cell(i+2,5).value)
+            BH_GWL.append(sheet_bhmgr.cell(i+2,6).value)
+            BH_cat.append("BH")
+    
+        BH_X = np.array(BH_X)
+        BH_Y = np.array(BH_Y)
+        BH_Z = np.array(BH_Z)
+        BH_GWL = np.array(BH_GWL)
+        BH_ID = np.array(BH_ID)
+        BH_cat = np.array(BH_cat)
+    
+        CPT_n = sheet_cptmgr.max_row - 1
+        CPT_ID, CPT_X, CPT_Y, CPT_Z, CPT_GWL, CPT_cat = [],[],[],[],[],[]
+        for i in range(CPT_n):
+            CPT_ID.append(sheet_cptmgr.cell(i+2,2).value)
+            CPT_X.append(sheet_cptmgr.cell(i+2,3).value)
+            CPT_Y.append(sheet_cptmgr.cell(i+2,4).value)
+            CPT_Z.append(sheet_cptmgr.cell(i+2,5).value)
+            CPT_GWL.append(sheet_cptmgr.cell(i+2,6).value) 
+            CPT_cat.append("CPT")
+    
+        CPT_X = np.array(CPT_X)
+        CPT_Y = np.array(CPT_Y)
+        CPT_Z = np.array(CPT_Z)
+        CPT_GWL = np.array(CPT_GWL)
+        CPT_ID = np.array(CPT_ID)
+        CPT_cat = np.array(CPT_cat)
+    
+        ALL_X = np.concatenate((BH_X, CPT_X))
+        ALL_Y = np.concatenate((BH_Y, CPT_Y))
+        ALL_Z = np.concatenate((BH_Z, CPT_Z))
+        ALL_GWL = np.concatenate((BH_GWL, CPT_GWL))
+        ALL_ID = np.concatenate((BH_ID, CPT_ID))
+        ALL_cat = np.concatenate((BH_cat, CPT_cat))
+    
+        ALL_X = [i for i in ALL_X if i != None]
+        ALL_Y = [i for i in ALL_Y if i != None]
+        ALL_Z = [i for i in ALL_Z if i != None]
+        ALL_GWL = [i for i in ALL_GWL if i != None]
+        ALL_ID = [i for i in ALL_ID if i != None]
+        ALL_cat = [i for i in ALL_cat if i != None]
+    
+        data = {'ID' : ALL_ID,
+                'X_UTM' : ALL_X,
+                'Y_UTM' : ALL_Y,
+                'Z' : ALL_Z,
+                'GWL' : ALL_GWL,
+                'CLASS' : ALL_cat}
+    
+        df = pd.DataFrame(data)
+    
+        latlon, lat, lon = [],[],[]
+        for i in range(len(ALL_X)):
+            #print(df['ID'].iloc[i])
+            if utm_hemi == 'North':
+                hemis = True
+            elif utm_hemi == 'South':
+                hemis = False
+            latlon.append(utm.to_latlon(ALL_X[i], ALL_Y[i], utm_zone, northern=hemis))
+            lat.append(latlon[i][0])
+            lon.append(latlon[i][1])
+    
+        df['lat']=lat
+        df['lon']=lon
+    
+        latmean = df['lat'].mean()
+        lonmean = df['lon'].mean()
+        location = [latmean, lonmean]
+        m = folium.Map(location=location, zoom_start=15
+                        # , tiles="CartoDB positron"
+                       )
+    
+        icons = []
+        for i in range(0,len(df)):
+            if df['CLASS'].iloc[i] == "BH":
+                icons.append(folium.Icon(color='black'))
+            elif df['CLASS'].iloc[i] == "CPT":
+                icons.append(folium.Icon(color='red'))
+    
+        for i in range(0,len(df)):
+            folium.Marker([df['lat'].iloc[i], df['lon'].iloc[i]], popup=df['ID'].iloc[i], icon=icons[i]).add_to(m)
+    
+        output = st_folium(m, width=1200, height=800, returned_objects=[])
+    
+    elif "wb" not in st.session_state:
+        pass
